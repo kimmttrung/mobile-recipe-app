b00a72f151cc54dd0540be846b0bf439bc70cc79
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-07-29 16:02:05 +0700

    setup assets

---
+++ app/about.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function about() {
+    return (
+        <View>
+            <Text>Hello trung</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
