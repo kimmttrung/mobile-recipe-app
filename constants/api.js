2a9ddc17bde654e5b7d8013e8b8d13997d376481
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-08-08 22:11:53 +0700

    complete project

---
+++ constants/api.js
@@ -0,0 +1,3 @@
+// export const API_URL = "http://localhost:5001/api";
+// export const API_URL = "http://192.17.12.8:5001/api";
+export const API_URL = "https://recipe-app-api-mq9u.onrender.com/api";
